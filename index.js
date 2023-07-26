3f39e929f17bb6993c3e5a316df9c11a2b60977a
Siddharth Mourya <mouryasiddharth98@gmail.com>
2023-07-26 15:54:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set 'trust proxy' to true to enable handling of proxy headers
+app.set('trust proxy', true);
 
 // Middleware to parse JSON in the request body
 app.use(express.json());
@@ -7,14 +11,17 @@ app.use(express.json());
 // POST API endpoint
 app.post('/api/data', (req, res) => {
   const { body } = req;
-  const ipAddress = req.ip; // Get the IP address from the request
+  const ipAddress = req.ip; // By default, this would be 127.0.0.1 if trust proxy is not set
+  const forwardedIps = req.ips; // Array of proxy server IP addresses
 
-  // Send back the IP address in the response
-  res.status(200).json({ message: 'Data received successfully! ', data: body, ipAddress });
+  console.log('Received data from IPs:', forwardedIps.length ? forwardedIps : [ipAddress]);
+  console.log('Data received:', body);
+
+  // Send back the IP addresses in the response
+  res.status(200).json({ message: 'Data received successfully!', data: body, ips: forwardedIps });
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
