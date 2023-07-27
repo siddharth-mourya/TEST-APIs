7df07173ed3f0ab248ca4a09eef2de8031d4a70c
Siddharth Mourya <mouryasiddharth98@gmail.com>
2023-07-27 12:17:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ app.post('/api/data', (req, res) => {
   res.status(200).json({ message: 'Data received successfully!', data: body, ips: forwardedIps });
 });
 
+// POST API endpoint
+app.get('/api/data', (req, res) => {
+  const ipAddress = req.ip; // By default, this would be 127.0.0.1 if trust proxy is not set
+  const forwardedIps = req.ips; // Array of proxy server IP addresses
+
+  console.log('Received data from IPs:', forwardedIps.length ? forwardedIps : [ipAddress]);
+
+  // Send back the IP addresses in the response
+  res.status(200).json({ message: 'Request received successfully!', ips: forwardedIps });
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
